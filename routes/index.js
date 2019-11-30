bf603e86403bcfbd6d5bf067a41d64630a11cacb
unknown <ebusameric@gmail.com>
2019-11-30 15:51:47 +0100

    features: add routes for all users and single user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const { user, details } = require('../controller');
 const { validators } = require('../middlewares');
 
-console.log(validators)
+
 module.exports = (app) => {
 	app.post('/api/user/register', validators.register, user.register);
 	app.post('/api/user/login', validators.login, user.login);
+	app.get('/api/users', user.users) // For Admin
+	app.get('/api/user/:id', user.account);
 }
