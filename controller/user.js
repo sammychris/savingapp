a432daa32c1377070f2415c83aef77f391284eaa
unknown <ebusameric@gmail.com>
2019-11-30 15:52:36 +0100

    feature: complete the login and register routes

--- controller/user.js
+++ controller/user.js
@@ -2,18 +2,41 @@ const db = require('../models');
 
 module.exports = {
 	register(req, res){
-		// db('test').insert(
-		// 	[{
-		// 		'firstname': 'samuel',
-		// 		'lastname': 'John',
-		// 		'email': 'ebusameric@gmail.com',
-		// 		'password': 'sammychris12'
-		// 	}]
-		// ).then(a => console.log('inserted successfully!'))
-		res.json({ message: 'This is the most amazing of all time!' });
+		const { firstname, lastname, email, password, phone_no, address } = req.body;
+		db('users').where({ email })
+			.then(user => {
+				// check if user already exits!
+				if(user[0]) return res.json({ message: 'User already exists!' });
+
+				// create new user
+				db('users').insert([{ firstname, lastname, email, password, phone_no, address }])
+					.then(() => res.json({ message: 'Registered successfully!' }));
+			});
 	},
 
 	login(req, res){
+		const { email, password } = req.body;
+		db('users').where({ email }).select('password')
+			.then(user => {
+				// check if user does not exits!
+				if(!user[0]) return res.json({ message: 'User does not exists!' });
 
+				// check if input password is equal to existed user password!
+				if(user[0].password !== password) return res.json({ message: '!wrong password!'});
+
+				// loggedin successs and create a token
+				return res.json({ message: 'creates a token and loggedin successfully!'})
+
+			});
+	},
+
+	account(req, res){
+
+	},
+
+	users(req, res){
+		db('users')
+			.then(a => res.json(a));
 	}
+
 }
