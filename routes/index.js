835dba0504342481a37cd4f72ac7defe07da696d
unknown <ebusameric@gmail.com>
2019-12-16 17:58:13 +0100

    features: add passport to route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
+const passport          = require('passport');
 const { user, details } = require('../controller');
-const { validators } = require('../middlewares');
+const { validators }    = require('../middlewares');
 
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//       return next();
+//   }
+//   res.redirect('/');
+// };
 
 module.exports = (app) => {
 	app.post('/api/user/register', validators.register, user.register);
 	app.post('/api/user/login', validators.login, user.login);
-	app.get('/api/users', user.users) // For Admin
+	app.get('/api/users', passport.authenticate('jwt', { session: false }), user.users) // For Admin
 	app.get('/api/user/:id', user.account);
 }
