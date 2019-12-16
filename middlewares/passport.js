aa2ea56ec07730f45d4f537e28e4084a9a77c1d7
unknown <ebusameric@gmail.com>
2019-12-16 20:09:39 +0100

    features: add the facebook passport strategy

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,8 +1,9 @@
-const passport    = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt    = require('passport-jwt').ExtractJwt;
-
-const db          = require('../models');
+const passport         = require('passport');
+const JwtStrategy      = require('passport-jwt').Strategy,
+    ExtractJwt         = require('passport-jwt').ExtractJwt;
+const FacebookStrategy = require('passport-facebook');
+const GoogleStrategy   = require('passport-google-oauth');
+const db               = require('../models');
 
 
 
@@ -17,6 +18,7 @@ module.exports = function (app) {
     // Initiallize Passport...
     app.use(passport.initialize());
 
+    // Jwt Strategy....
     passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
         db('users').where({ id: jwt_payload.id })
             .then(user => {
@@ -26,4 +28,20 @@ module.exports = function (app) {
             })
             .catch(err => done(err, false));
     }));
+
+
+    // Facebook Strategy
+    passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "http://localhost:8000/auth/facebook/callback",
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        console.log(refreshToken );
+        // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        //   return cb(err, user);
+        // });
+        cb('samuel')
+      }
+    ));
 }
