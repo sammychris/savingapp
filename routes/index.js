8ff31d6856e8822a2b29b59758b20944e0d98564
unknown <ebusameric@gmail.com>
2019-12-16 20:10:14 +0100

    features: add facebook strategy to routes

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,14 @@ module.exports = (app) => {
 	app.post('/api/user/login', validators.login, user.login);
 	app.get('/api/users', passport.authenticate('jwt', { session: false }), user.users) // For Admin
 	app.get('/api/user/:id', user.account);
+
+	app.get('/auth/facebook', passport.authenticate('facebook'));
+
+	app.get('/auth/facebook/callback',
+	  passport.authenticate('facebook', { failureRedirect: '/login' }),
+		  function(req, res) {
+		  	console.log('welcome sir')
+		    // Successful authentication, redirect home.
+		    res.redirect('/');
+		  });
 }
