25dd4096f5ce5c588d4bbeb617e03b093cca2e06
unknown <ebusameric@gmail.com>
2019-12-16 18:00:35 +0100

    features: add bcrypt for password hashed and create token

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,7 @@
-const db = require('../models');
+const jwt    = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const db     = require('../models');
+
 
 module.exports = {
 	register(req, res){
@@ -8,24 +11,44 @@ module.exports = {
 				// check if user already exits!
 				if(user[0]) return res.json({ message: 'User already exists!' });
 
-				// create new user
-				db('users').insert([{ firstname, lastname, email, password, phone_no, address }])
-					.then(() => res.json({ message: 'Registered successfully!' }));
+				bcrypt.hash(password, 12).then(hash => {
+
+				  // create new user
+					db('users').insert([{ firstname, lastname, email, password: hash, phone_no, address }])
+						.then(() => res.json({ message: 'Registered successfully!' }));
+
+				}).catch(err => console.error(err));
+
 			});
 	},
 
 	login(req, res){
 		const { email, password } = req.body;
-		db('users').where({ email }).select('password')
-			.then(user => {
+		db('users').where({ email })
+			.then(users => {
+				const user = users[0];
+				const pay_load = {};
+
 				// check if user does not exits!
-				if(!user[0]) return res.json({ message: 'User does not exists!' });
+				if(!user) return res.json({ message: 'User does not exists!' });
 
-				// check if input password is equal to existed user password!
-				if(user[0].password !== password) return res.json({ message: '!wrong password!'});
+				// compare password
+    		bcrypt.compare(password, user.password).then(passed => {
+				  // passed is equal to false...
+				  if(!passed) return res.json({ message: '!Wrong Password!'});
 
-				// loggedin successs and create a token
-				return res.json({ message: 'creates a token and loggedin successfully!'})
+				  pay_load.id 				= user.id,
+			    pay_load.firstname 	= user.firstname;
+			    pay_load.lastname 	= user.lastname;
+			    pay_load.email 			= user.email;
+			    pay_load.password 	= user.password;
+
+					// loggedin successs and create a token
+					const token = jwt.sign(pay_load, process.env.JWT_SECRET);
+
+				  return res.json({ user, token });
+
+				}).catch(err => console.error(err));
 
 			});
 	},
