44490b542631bc3681527ffa0858011e0dad48d1
unknown <ebusameric@gmail.com>
2019-12-18 18:35:45 +0100

    features: add a module for homepage

---
+++ client/src/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Homepage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Welcome to react saving application...</h1>
+			</div>
+			);
+	}
+}
+
+export default Homepage;
