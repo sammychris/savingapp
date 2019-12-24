0c9fcfe90fadc02e6735bbb996726a76310d2f22
unknown <ebusameric@gmail.com>
2019-12-24 14:52:23 +0100

    features: add an authenticate page route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landingpage from './Landingpage';
 import Loginpage from './Loginpage';
 import Registerpage from './Registerpage';
 import Userpage from './Userpage';
+import Authpage from './Authpage';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Navigation } from './Components';
 import { ProtectedRoute, PublicRoute } from './helpers';
@@ -15,11 +16,12 @@ function App() {
       <Navigation />
       <div className="App">
         <Switch>
-        <PublicRoute exact path="/" component={Landingpage}/>
-        <PublicRoute exact path="/login" component={Loginpage}/>
-        <PublicRoute exact path="/register" component={Registerpage}/>
-        <ProtectedRoute exact path="/dashboard" component={Userpage} />
-        <Route path="*" component={() => '404 Page Not Found!'} />
+          <PublicRoute exact path="/" component={Landingpage}/>
+          <PublicRoute exact path="/login" component={Loginpage}/>
+          <PublicRoute exact path="/register" component={Registerpage}/>
+          <PublicRoute exact path="/authenticated" component={Authpage} />
+          <ProtectedRoute exact path="/dashboard" component={Userpage} />
+          <Route path="*" component={() => '404 Page Not Found!'} />
         </Switch>
       </div>
     </Router>
