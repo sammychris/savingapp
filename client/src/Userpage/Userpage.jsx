4c1edbdf180a57f8aa32429ece387e11bfeef4ff
unknown <ebusameric@gmail.com>
2019-12-25 16:29:58 +0100

    chores: remove an unwanted link

--- client/src/Userpage/Userpage.jsx
+++ client/src/Userpage/Userpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Jumbotron } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { getUsers } from '../services';
 
 
