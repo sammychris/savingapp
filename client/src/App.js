024c73b59c819f7063fdaf0b9f700b8cb0c1458f
unknown <ebusameric@gmail.com>
2020-01-01 17:31:20 +0100

    features: add more react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
-import Landingpage from './Landingpage';
-import Loginpage from './Loginpage';
-import Registerpage from './Registerpage';
-import Userpage from './Userpage';
-import Authpage from './Authpage';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Navigation } from './Components';
-import { ProtectedRoute, PublicRoute } from './helpers';
+//import Landingpage from './Landingpage';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { ProtectedRoute, PublicRoute, Authpage } from './helpers';
+
+
+
+import UserLayout from "layouts/User.jsx";
+import VisitorLayout from "layouts/Visitor.jsx";
 
 
 function App() {
   return (
     <Router>
-      <Navigation />
       <div className="App">
         <Switch>
-          <PublicRoute exact path="/" component={Landingpage}/>
-          <PublicRoute exact path="/login" component={Loginpage}/>
-          <PublicRoute exact path="/register" component={Registerpage}/>
+          <Route exact path="/" render={props => <VisitorLayout {...props} />} />
+          <Route path="/auth/login" render={props => <VisitorLayout {...props} />} />
           <PublicRoute exact path="/authenticated" component={Authpage} />
-          <ProtectedRoute exact path="/dashboard" component={Userpage} />
+          <Route path="/user" render={props => <UserLayout {...props} />} />
           <Route path="*" component={() => '404 Page Not Found!'} />
         </Switch>
       </div>
